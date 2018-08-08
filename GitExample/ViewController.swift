36cfd3bd8728acf1de3ef5fcfa8948c9d4941c29
Meisam Rezaei <meysam.or.us@gmail.com>
2018-08-08 16:41:00 +0430

    Initial Commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Meysam Rezaeei on 8/8/18.
+//  Copyright © 2018 Meysam Rezaeei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
